b867b17aee4103cb82d73c29bc046abec70693a6
TheodorTomas <theodortomas@gmail.com>
2017-11-03 19:12:44 +0100

    Changing color in animation

--- internals/BackgroundEffect/BackgroundEffect.js
+++ internals/BackgroundEffect/BackgroundEffect.js
@@ -173,7 +173,7 @@
             ctx.beginPath();
             ctx.moveTo(p.x, p.y);
             ctx.lineTo(p.closest[i].x, p.closest[i].y);
-            ctx.strokeStyle = 'rgba(158,158,158,'+ p.active+')';
+            ctx.strokeStyle = 'rgba(149,163,179,'+ p.active+')';
             ctx.stroke();
         }
     }
@@ -192,7 +192,7 @@
             if(!_this.active) return;
             ctx.beginPath();
             ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(92,219,231,'+ _this.active+')';
+            ctx.fillStyle = 'rgba(120,200,180,'+ _this.active+')';
             ctx.fill();
         };
     }
